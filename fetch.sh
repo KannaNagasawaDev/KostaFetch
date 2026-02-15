8afb2b374f22cb0e5f6b380f0dbb03d3573a9016
Kanna Nagasawa <sairenyaa@gmail.com>
2026-02-15 14:32:50 +0100

    add shell info

--- fetch.sh
+++ fetch.sh
@@ -4,6 +4,7 @@ bash -c '
     user_host="$(whoami)@$(cat /etc/hostname 2>/dev/null || echo localhost)"
     os_name="$(grep "^NAME=" /etc/os-release | cut -d= -f2 | tr -d \")"
     kernel="$(uname -r)"
+    shell="$(basename "$SHELL")"
     uptime="$(awk "{h=int(\$1/3600); m=int((\$1%3600)/60); printf \"%dh %dm\", h, m}" /proc/uptime)"
     cpu="$(grep -m1 "model name" /proc/cpuinfo | cut -d: -f2 | xargs)"
     gpu="$(for d in /sys/bus/pci/devices/*; do 
@@ -19,6 +20,7 @@ bash -c '
     echo -e "\e[38;5;141muser@host:\e[0m $user_host"
     echo -e "\e[38;5;141mos:\e[0m $os_name"
     echo -e "\e[38;5;141mkernel:\e[0m $kernel"
+    echo -e "\e[38;5;141mshell:\e[0m $shell"
     echo -e "\e[38;5;141muptime:\e[0m $uptime"
     echo -e "\e[38;5;141mcpu:\e[0m $cpu"
     echo -e "\e[38;5;141mgpu:\e[0m $gpu"
